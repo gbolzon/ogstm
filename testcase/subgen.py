a08e8520c7097af7697c3d0ae851c54bbf45f31a
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2017-03-21 11:23:36 +0100

    Adding testcase, imported from master

---
+++ testcase/subgen.py
@@ -0,0 +1,12 @@
+# generates submask.nc for createGridDA
+# to be executed in MODEL/ or where meshmask.nc is
+
+from basins import V0 as OGS
+from commons.submask import SubMask
+from commons.mask import Mask
+TheMask=Mask('meshmask.nc')
+
+for sub in OGS.Pred:
+    S=SubMask(sub,maskobject=TheMask)
+    S.save_as_netcdf('submask.nc', maskvarname=sub.name)
+
