597158a5299463bc311bf4742acd8c348dea68a9
Gianluca Coidessa <gcoidess@r033c01s04.galileo.cineca.it>
2020-11-09 12:35:57 +0100

    changing for multi processor per node

--- src/MPI/nodes_module.f90
+++ src/MPI/nodes_module.f90
@@ -34,6 +34,8 @@ MODULE NODES_MODULE
         INTEGER :: nodes
         
         INTEGER, allocatable, dimension (:) :: writing_procs
+        INTEGER, allocatable, dimension (:) :: writing_procs_base
+        INTEGER :: num_of_wr_procs_perNODE
         
         CONTAINS
 
@@ -44,6 +46,7 @@ MODULE NODES_MODULE
         INTEGER :: IERROR
         CHARACTER*(MPI_MAX_PROCESSOR_NAME) buff_recv
         
+        INTEGER ::  cycle_cont
 
         CHARACTER (len = lengt), dimension(mpi_glcomm_size) :: total_array
         
@@ -90,16 +93,17 @@ MODULE NODES_MODULE
         !determing how many processor are inside first node, each node, delta numberby counting inside total array
         !creating array of writing nodes
 
-                ALLOCATE (writing_procs(nodes))        
+
+                ALLOCATE (writing_procs_base(nodes))        
         
-                writing_procs(1) = 0
+                writing_procs_base(1) = 0
                 DO i=2, mpi_glcomm_size
                         IF (nodes==1) THEN
                                 !if the number of node is one break, no sense to calculate, avoid problems
                                 EXIT
                         
                         ELSE IF (total_array(p) /= total_array(i)) THEN
-                                writing_procs(k)= i-1
+                                writing_procs_base(k)= i-1
                                 p=i
                                 k=k+1
                         ELSE
@@ -107,9 +111,33 @@ MODULE NODES_MODULE
                         END IF
                 END DO                
 
+        
+        ! cycle to fill writing procs array with num_of_wr_procs_perNODE per
+        ! node
+                
+                ALLOCATE (writing_procs(nodes*num_of_wr_procs_perNODE))
+                
+                num_of_wr_procs_perNODE = 2
+       
+                cycle_cont = 0
+
+                DO i=1,nodes
+                        IF (nodes==1) THEN
+                                !if the number of node is one break, no sense to
+                                !calculate, avoid problems
+                                EXIT
+                        ELSE
+                                writing_procs(i + cycle_cont) = writing_procs_base(i)
+                                writing_procs(i + cycle_cont +1) = writing_procs_base(i) + 20
+                                cycle_cont = cycle_cont + 1
+                        END IF
+                END DO
+                
+
         !rank 0 send to all ranks writing_procs
 
                 DO i=1, mpi_glcomm_size - 1
+                        CALL MPI_Send(num_of_wr_procs_perNODE,1,MPI_INT,i,8,MPI_COMM_WORLD,IERROR)
                         CALL MPI_Send(writing_procs,nodes,MPI_INT,i,3,MPI_COMM_WORLD,IERROR)
                 END DO
 
@@ -121,8 +149,9 @@ MODULE NODES_MODULE
         IF (myrank >0) THEN
                 
                 CALL MPI_Recv(nodes,1,MPI_INT,0,4,MPI_COMM_WORLD,MPI_STATUS_IGNORE, IERROR)
+                CALL MPI_Recv(num_of_wr_procs_perNODE,1,MPI_INT,0,8,MPI_COMM_WORLD,MPI_STATUS_IGNORE, IERROR)
                 
-                ALLOCATE (writing_procs(nodes))
+                ALLOCATE (writing_procs(nodes*num_of_wr_procs_perNODE))
 
                 CALL MPI_Recv(writing_procs,nodes,MPI_INT,0,3,MPI_COMM_WORLD,MPI_STATUS_IGNORE, IERROR)
 
